33b892537d2a776cd45b57c635183cfe3e50daac
MauricioNC <mauricionc19@gmailc.om>
2023-02-03 22:57:20 -0600

    Create new department functionality completed.

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -4,4 +4,20 @@ class DepartmentsController < ApplicationController
   def new
     @department = Department.new
   end
+
+  def create
+    @department = Department.new(department_params)
+
+    if @department.save
+      redirect_to root_path, success: "The department #{@department.department} was created successfully."
+    else
+      render :new, error: @department.error.full_messages
+    end
+  end
+
+  private
+
+  def department_params
+    params.require(:department).permit(:department, :manager)
+  end
 end
