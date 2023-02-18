1857fda8be2ecdaf6ee6963194c4f9168720be38
MauricioNC <mauricionc19@gmailc.om>
2023-02-17 21:43:15 -0600

    Absences task programmed
    The below steps describes how to execute the method to create a new absence automatically:
    
    - To do this, was necessary add to the Gemfile the whenever gem.
      Following the instruction in the official documentation, and after run
      some commands mentioned in the documentation, a schedule.rb file is
      created in /config.
      This file should contains the instruction to execuete a method every n
      time, this could be every minute, hour, day, week, etc.
      To call a method from a model, is necessary follow the sintaxis
      "model_name.method_name", after specified every which time the mehtod
      will be execueted, the instruction runner executes the method, we can
      add a 'puts' instruction inside the string to see the response.
      To be able to see the response, it's necessary set the log file, in
      this case is called 'cron.log'.
      Finally, to make this possible, it's necessary set to false the
      insctruction 'config.cache_classes' in the ./config/application.rb, by
      this way, the code will be reloaded when something change.
    
    Other changes applied
    
    - absences_controller: The method to create a new absence was removed
      from this controller and was inserted into the absence model, this
      because the whenever gem run the task from the model.
    - employees_schedules_controller: Was restructured the validations due
      to an multiple redirect_to calls error.
    - employee_schedules_helper: Was removed the error message when one or
      more employees has an absence, just return false for none employees
      absence or true if there are one or more employees absence.
    - absence model: As mentioned above, the create method for an absence
      was put here.

--- app/controllers/absences_controller.rb
+++ app/controllers/absences_controller.rb
@@ -1,15 +1,2 @@
 class AbsencesController < ApplicationController
-  protect_from_forgery with: :null_session
-  def create
-    today_date = Date.today.to_time
-    @employees_employee_schedule = Employee.joins(:employee_schedules).where.not("check_in >= ?", today_date).distinct
-
-    @employees_employee_schedule.each do |employee|
-      @absence_employee = employee.absences.new(date_of_absence: today_date)
-      unless @absence_employee.save
-        redirect_to check_in_path, error: "Something went wrong registering absences."
-      end
-    end
-    flash[:success] = "All the absences were registered successfully at #{today_date}"
-  end
 end
