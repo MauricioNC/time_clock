5356210033566d454d74c4ca47930e9c32824b4b
MauricioNC <mauricionc19@gmailc.om>
2023-02-21 15:10:36 -0600

    Added gender field to employees table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_17_042111) do
+ActiveRecord::Schema.define(version: 2023_02_21_210612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2023_02_17_042111) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "department_id", null: false
+    t.string "gender"
     t.index ["department_id"], name: "index_employees_on_department_id"
   end
 
