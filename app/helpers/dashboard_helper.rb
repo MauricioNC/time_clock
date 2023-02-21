bb7684fcb76120f57adb5586e6cd582985a9ef6b
MauricioNC <mauricionc19@gmailc.om>
2023-02-21 15:27:52 -0600

    Top cards
    Was completed the top cards dinamically information, was applied the
    next changes in the dashboard helper:
    
    - Was created the get_total_employees method, just return the total employees.
    - Was created the get_total_absences method.
    - Was created the get_total_employee_schedules, this method is used in
      the next method.
    - Was created the avg_early method, this mehtos has te logic to get the
      total average to arrived early at work.
    - Finally, was created the get_gender_percentage, that receives a
      parameter, that is the gender percentage that will be returned.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,4 +3,36 @@ module DashboardHelper
     department = Department.find_by(department: department_name)
     department.employees.count
   end
+
+  def get_total_employees
+    Employee.all.count
+  end
+
+  def get_total_absences
+    Absence.all.count
+  end
+
+  def get_total_employee_schedules
+    EmployeeSchedule.joins(:employee)
+  end
+
+  def avg_early
+    employee_schedules = get_total_employee_schedules
+    total_late_arrivals = employee_schedules.select { |e| (e.check_in.strftime("%H:%M:%S") > "09:00:00" && e.check_in.strftime("%H:%M:%S %z") < "09:16:00") == false }
+    days_of_current_month = Time.days_in_month(Time.now.month, Time.now.year) - 8
+    total_arrivals = (get_total_employees * days_of_current_month)
+    ((total_late_arrivals.count * 100) / total_arrivals)
+  end
+
+  def get_gender_percentage(gender)
+    female = Employee.where(gender: "Female").count
+    male = Employee.where(gender: "Male").count
+
+    return (female * 100) / get_total_employees if gender == "Female"
+    return (male * 100) / get_total_employees if gender == "Male"
+  end
+
+  def get_male_gender
+    Employee.where(gender: "Male")
+  end
 end
